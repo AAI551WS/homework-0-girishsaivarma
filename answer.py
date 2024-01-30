e8ab31ef0daa63e415f58df6489e64e5e65b5b52
girishsaivarma <151422355+girishsaivarma@users.noreply.github.com>
2024-01-29 20:01:08 -0500

    Update answer.py

--- answer.py
+++ answer.py
@@ -2,6 +2,7 @@ def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
     """
+print("Hello, World!")
 
     """
 
@@ -12,6 +13,6 @@ def hello_world():
 def add(a,b):
 
     # Write simple code to add variable a and b
-    output =
+    output = a+b
 
     return output
